4f51a0278949e3831fe5e8ceb7cf4b507272ce12
jesuisnicolas <nschaberger@outlook.com>
2020-09-18 17:18:52 -0300

    First Draft

---
+++ js/lastmodified.js
@@ -0,0 +1,9 @@
+var modDate= document.lastModified;
+document.getElementById("lastMod").innerHTML= modDate;
+
+function wait(){
+    alert ("Too Soon! Wait another week!");
+}
+function wait2(){
+    alert ("Way too Soon! Wait two weeks!");
+}

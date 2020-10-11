50b0a6bb9b982259b06c36daaa44011530f4b163
jesuisnicolas <nschaberger@outlook.com>
2020-10-11 01:10:37 -0300

    preston 0.1

---
+++ lesson4/js/lastModified.js
@@ -0,0 +1,4 @@
+// var modDate= document.lastModified;
+var year= new Date();
+// document.getElementById("lastMod").innerHTML= modDate;
+document.getElementsByClassName("copyYear").innerHTML= year.getFullYear();

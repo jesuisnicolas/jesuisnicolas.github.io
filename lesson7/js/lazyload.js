9f956939264a430666911f46ac4e71a2c04234fc
jesuisnicolas <nschaberger@outlook.com>
2020-11-03 18:17:31 -0300

    fixed lazyload

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,8 +1,8 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imgOptions = {
-    threshold: 1, 
-    rootMargin: "0px 0px 50px 0px"
+  threshold: .2,
+  rootMargin: "0px 0px -150px 0px"
 };
 
 const loadImages = (image) => {
@@ -12,20 +12,20 @@ const loadImages = (image) => {
   };
 };
 
-  if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
     });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
+  }, imgOptions);
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
